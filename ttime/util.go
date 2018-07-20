8c62a5334949e638b8a247d201d26ba5b87e1ddd
Tannhauser <tannhauser.sphinx@gmail.com>
2018-07-20 11:17:18 +0800

    [Update] update ttime

--- ttime/util.go
+++ ttime/util.go
@@ -11,3 +11,7 @@ func GetDate() (string) {
   return fmt.Sprintf("%s", time.Now().UTC().Format("2006-01-02T15:04:05Z"));
 }
 
+func GetTimestampDate() (int64, string) {
+  now := time.Now();
+  return now.Unix(), fmt.Sprintf("%s", now.UTC().Format("2006-01-02T15:04:05Z"));
+}
