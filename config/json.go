e6d6f9beb1daa5d1bb084d089802a33440d41539
tannhauser <tannhauser.sphinx@gmail.com>
2018-07-26 14:16:06 +0800

    [New] add mutable json config

--- config/json.go
+++ config/json.go
@@ -1,27 +1,30 @@
-package config;
+package config
 
-import "encoding/json";
-import "os";
+import "encoding/json"
+import "os"
 
-type Error string;
+type Error string
 
-const ErrConf = Error("Failed to get conf file");
-const ErrInvalidField = Error("Got invalid key in conf file");
-const ErrInvalidValue = Error("Got invalid value in conf file");
-const ErrInvalidType = Error("Got invalid type in conf file");
+const ErrConf = Error("Failed to get conf file")
+const ErrInvalidField = Error("Got invalid key in conf file")
+const ErrInvalidValue = Error("Got invalid value in conf file")
+const ErrInvalidType = Error("Got invalid type in conf file")
 
-/* Initiate configuration, pass path to conf file and return a configuration entity */
-func InitJSONConf(path string, conf interface{}) (error) {
-  fp, err := os.Open(path);
-  defer fp.Close();
-  if err != nil {
-    return err;
-  }
-  decoder := json.NewDecoder(fp);
-  err = decoder.Decode(conf);
-  if err != nil {
-    return err;
-  }
-  return nil;
+func (e Error) Error() string {
+	return string(e)
 }
 
+/* Initiate configuration, pass path to conf file and return a configuration entity */
+func InitJSONConf(path string, conf interface{}) error {
+	fp, err := os.Open(path)
+	defer fp.Close()
+	if err != nil {
+		return err
+	}
+	decoder := json.NewDecoder(fp)
+	err = decoder.Decode(conf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
